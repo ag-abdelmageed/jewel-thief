070de587608c79e973fb486c916e4ead47543bfe
Mackenzie Scott <mscott3327@gmail.com>
2022-09-26 19:42:59 -0400

    Stationary Bombs

--- main.js
+++ main.js
@@ -157,15 +157,16 @@ function collectStar(player, star) {
       child.enableBody(true, child.x, 0, true, true);
     });
 
-    var x =
+    /*var x =
       player.x < 400
         ? Phaser.Math.Between(400, 800)
         : Phaser.Math.Between(0, 400);
-
-    var bomb = bombs.create(x, 16, "bomb");
+*/
+    var bomb = bombs.create(200, 16, "bomb");
+    bomb = bombs.create(500, 200, "bomb");
     bomb.setBounce(1);
     bomb.setCollideWorldBounds(true);
-    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    //bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
     bomb.allowGravity = false;
   }
 }
