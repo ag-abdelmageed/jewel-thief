1444259980a3d3326652de76c4373030f7b8f966
Mackenzie Scott <mscott3327@gmail.com>
2022-09-26 20:02:11 -0400

    Add bomb stillness

--- main.js
+++ main.js
@@ -17,20 +17,20 @@ var config = {
 };
 
 var player;
-var stars;
+var jewels;
 var bombs;
 var platforms;
 var cursors;
-var score = 0;
+//var score = 0;
 var gameOver = false;
-var scoreText;
+//var scoreText;
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("sky", "assets/sky.png");
+  this.load.image("checkerboard", "assets/checkerboard.png");
   this.load.image("ground", "assets/platform.png");
-  this.load.image("star", "assets/star.png");
+  this.load.image("jewel", "assets/jewel.png");
   this.load.image("bomb", "assets/bomb.png");
   this.load.spritesheet("dude", "assets/dude.png", {
     frameWidth: 32,
@@ -40,7 +40,7 @@ function preload() {
 
 function create() {
   //  A simple background for our game
-  this.add.image(400, 300, "sky");
+  this.add.image(400, 300, "checkerboard");
 
   //  The platforms group contains the ground and the 2 ledges we can jump on
   platforms = this.physics.add.staticGroup();
@@ -85,33 +85,25 @@ function create() {
   //  Input Events
   cursors = this.input.keyboard.createCursorKeys();
 
-  //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-  stars = this.physics.add.group({
-    key: "star",
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 },
-  });
+  jewel = this.physics.add.sprite(100, 100, "jewel");
+  jewel.setScale(0.04);
 
-  stars.children.iterate(function (child) {
-    //  Give each star a slightly different bounce
-    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  });
 
   bombs = this.physics.add.group();
 
   //  The score
-  scoreText = this.add.text(16, 16, "score: 0", {
-    fontSize: "32px",
-    fill: "#000",
-  });
+  //scoreText = this.add.text(16, 16, "score: 0", {
+    //fontSize: "32px",
+    //fill: "#000",
+ // });
 
   //  Collide the player and the stars with the platforms
   this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(jewel, platforms);
   this.physics.add.collider(bombs, platforms);
 
   //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-  this.physics.add.overlap(player, stars, collectStar, null, this);
+  this.physics.add.overlap(player, jewel, collectJewel, null, this);
 
   this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
@@ -144,32 +136,26 @@ function update() {
   }
 }
 
-function collectStar(player, star) {
-  star.disableBody(true, true);
+function collectJewel(player, jewel) {
+  jewel.disableBody(true, true);
 
   //  Add and update the score
-  score += 10;
-  scoreText.setText("Score: " + score);
+ // score += 10;
+  //scoreText.setText("Score: " + score);
 
-  if (stars.countActive(true) === 0) {
-    //  A new batch of stars to collect
-    stars.children.iterate(function (child) {
-      child.enableBody(true, child.x, 0, true, true);
-    });
 
-    /*var x =
+    var x =
       player.x < 400
         ? Phaser.Math.Between(400, 800)
         : Phaser.Math.Between(0, 400);
-*/
-    var bomb = bombs.create(200, 16, "bomb");
-    bomb = bombs.create(500, 200, "bomb");
+
+    var bomb = bombs.create(x, 16, "bomb");
     bomb.setBounce(1);
     bomb.setCollideWorldBounds(true);
-    //bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
     bomb.allowGravity = false;
   }
-}
+
 
 function hitBomb(player, bomb) {
   this.physics.pause();
